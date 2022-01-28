c984f8cb8a9b36a76cf286f28048f8160281e122
Marcelo Gaioso Werneck <sonata.br@gmail.com>
2022-01-28 14:02:01 -0300

    Remove unused code

--- lambdas/s3/open-c-lifs/object_created/new_model/app.py
+++ lambdas/s3/open-c-lifs/object_created/new_model/app.py
@@ -1,8 +1,8 @@
 import json
 import boto3
-import os
 
 s3 = boto3.resource('s3')
+
 
 def get_object(bucket_name: str, key: str):
     bucket = s3.Bucket(bucket_name)
@@ -10,10 +10,6 @@ def get_object(bucket_name: str, key: str):
     return obj.get()['Body'].read().decode('utf-8')
 
 def lambda_handler(event, context):
-    print('####################################')
-    print(f'event = {event}')
-    print('####################################')
-
     record = event['Records'][0]['s3']
     data = get_object(record['bucket']['name'], record['object']['key'])
     print(f'data = {data}')
